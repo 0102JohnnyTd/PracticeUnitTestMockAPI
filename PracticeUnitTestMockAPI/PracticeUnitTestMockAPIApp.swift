3bb56949d3adbfc8bff4e2398582cf05dac3f83d
0102johnny <rt2824.recruit.1022@gmail.com>
2023-06-01 23:51:11 +0900

    Initial commit

---
+++ PracticeUnitTestMockAPI/PracticeUnitTestMockAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeUnitTestMockAPIApp.swift
+//  PracticeUnitTestMockAPI
+//
+//  Created by Johnny Toda on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct PracticeUnitTestMockAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
