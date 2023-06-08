238672be73c6a39f4659c0e152c4943f9fa80fc8
0102johnny <rt2824.recruit.1022@gmail.com>
2023-06-08 17:01:54 +0900

    ポケモンの詳細を表示するPokemonDetailViewを作成

---
+++ PracticeUnitTestMockAPI/PokemonDetailView.swift
@@ -0,0 +1,44 @@
+//
+//  PokemonDetailView.swift
+//  PracticeUnitTestMockAPI
+//
+//  Created by Johnny Toda on 2023/06/08.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    let pokemon: Pokemon
+    @State var pokemonDetail: PokemonDetail?
+    @State var errorText: String?
+    let api = API()
+
+    var body: some View {
+        VStack {
+            if let pokemonDetail = pokemonDetail {
+                PokemonImageView(imageURL: URL(string: pokemonDetail.sprites.frontDefault ?? ""))
+                Text("Name: \(pokemonDetail.name)")
+                Text("Height: \(pokemonDetail.height)")
+                Text("Weight: \(pokemonDetail.weight)")
+            } else if let errorText {
+                Text(errorText)
+            } else {
+                ProgressView("Loading...")
+            }
+        }
+        .task {
+            do {
+                pokemonDetail = try await api.fetchPokemonDetail(pokemon: pokemon)
+            } catch {
+                errorText = error.localizedDescription
+            }
+        }
+        .navigationTitle("Pokemon Detail")
+    }
+}
+
+struct PokemonDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonDetailView(pokemon: Pokemon(name: "", url: ""))
+    }
+}
