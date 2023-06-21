a4ee430435c375fd64078c43511385a36dd03006
0102johnny <rt2824.recruit.1022@gmail.com>
2023-06-21 10:06:47 +0900

    [change] testCheckHttpErrorMessageメソッドを編集

--- PracticeUnitTestMockAPITests/PokemonListViewModelTests.swift
+++ PracticeUnitTestMockAPITests/PokemonListViewModelTests.swift
@@ -10,16 +10,16 @@ import Combine
 @testable import PracticeUnitTestMockAPI
 
 final class PokemonListViewModelTests: XCTestCase {
-    private var subscriptions = Set<AnyCancellable>()
-
     // 取得したポケモンデータのテスト
     func testPokemonList() async throws {
+        var subscriptions = Set<AnyCancellable>()
         let expectation = expectation(description: "pokemonList")
         let viewModel = PokemonListViewModel(api: MockAPI())
 
         viewModel.$pokemonList
             .dropFirst()
             .prefix(1)
+            .receive(on: RunLoop.main)
             .sink { pokemonList in
                 XCTAssertEqual(pokemonList?.results[18].name, "rattata")
                 XCTAssertEqual(pokemonList?.results[18].url, "https://pokeapi.co/api/v2/pokemon/19/")
@@ -33,16 +33,35 @@ final class PokemonListViewModelTests: XCTestCase {
 
         // 通信エラー時のテスト
         @MainActor
-        func testCheckHttpErrorMessage() async throws {
-            // 通信環境なしで通信を実行した場合に発生するエラーを固定値として返すViewModelを生成
-            let viewModel = PokemonListViewModel(api: MockAPI(httpError: .noNetwork))
-            viewModel.fetchPokemonList()
-            XCTContext.runActivity(named: "HTTPErrorに関して") { _ in
-                XCTContext.runActivity(named: ".noNetWorkが生じた場合") { _ in
-                    XCTAssertEqual(viewModel.errorMMessage, "DEBUG (noNetwork): A network connection could not be established.")
-                }
+    func testCheckHttpErrorMessage() async throws {
+        // 通信環境なしで通信を実行した場合に発生するエラーを固定値として返すViewModelを生成
+        let viewModel = PokemonListViewModel(api: MockAPI(httpError: .noNetwork))
+        viewModel.fetchPokemonList()
+        XCTContext.runActivity(named: "HTTPErrorに関して") { _ in
+            XCTContext.runActivity(named: ".noNetWorkが生じた場合") { _ in
+                var subscriptions = Set<AnyCancellable>()
+
+                let expectation = expectation(description: "errorMMessage")
+
+                // 通信環境なしで通信を実行した場合に発生するエラーを固定値として返すViewModelを生成
+                let viewModel = PokemonListViewModel(api: MockAPI(httpError: .noNetwork))
+
+                viewModel.$errorMMessage
+                    .dropFirst()
+                    .prefix(1)
+                    .receive(on: RunLoop.main)
+                    .sink { errorMessage in
+                        XCTAssertEqual(errorMessage, "DEBUG (noNetwork): A network connection could not be established.")
+                        expectation.fulfill()
+                    }.store(in: &subscriptions)
+
+                viewModel.fetchPokemonList()
+
+                wait(for: [expectation], timeout: 5)
             }
+            XCTAssertEqual(viewModel.errorMMessage, "DEBUG (noNetwork): A network connection could not be established.")
         }
+    }
 
     // パース失敗時のテスト
     @MainActor
