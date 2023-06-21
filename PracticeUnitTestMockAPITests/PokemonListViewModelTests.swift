b962b2433f68238c409d34f8a6169e8bffac0835
0102johnny <rt2824.recruit.1022@gmail.com>
2023-06-21 10:21:04 +0900

    [refactor] testPokemonListメソッドで不要となったasyncキーワードを削除

--- PracticeUnitTestMockAPITests/PokemonListViewModelTests.swift
+++ PracticeUnitTestMockAPITests/PokemonListViewModelTests.swift
@@ -11,7 +11,7 @@ import Combine
 
 final class PokemonListViewModelTests: XCTestCase {
     // 取得したポケモンデータのテスト
-    func testPokemonList() async throws {
+    func testPokemonList() throws {
         var subscriptions = Set<AnyCancellable>()
         let expectation = expectation(description: "pokemonList")
         let viewModel = PokemonListViewModel(api: MockAPI())
@@ -79,6 +79,3 @@ final class PokemonListViewModelTests: XCTestCase {
         }
     }
 }
-
-
-
